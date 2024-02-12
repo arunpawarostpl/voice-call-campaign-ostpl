4e5d8ecf304d525c7aa0f057ff6d66604f932572
arunpawarostpl <147585117+arunpawarostpl@users.noreply.github.com>
2024-02-12 15:15:38 +0530

    fix report

--- src/models/report.js
+++ src/models/report.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 // Define a schema for API hits
 const campaignreportSchema = new mongoose.Schema({
     campaignRefId: { type: String, required: true, unique: true },
-    count: { type: Number, default: 0 },
-    responses: [{ type: String }],
+    hits: [{
+        count: { type: Number, default: 0 },
+        responses: [String],
+      }],
 });
 
 const campaignReport = mongoose.model('campaignreport', campaignreportSchema);
